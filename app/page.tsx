6d725375575592832fd473bcfc8be0e2e64dbd55
daryl <daryl@macmini.local>
2023-09-18 09:45:43 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,15 @@ interface DetailProps {
   age: number;
 }
 
-createStore({
-  theDetail: { name: 'bill', age: 16 },
-});
+createStore(
+  {
+    theDetail: { name: 'bill', age: 16 },
+  },
+  {
+    persist: 'action',
+    storageType: localStorage,
+  }
+);
 
 // function updateName(state: { theDetail: DetailProps }, payload: DetailProps) {
 //   return {
@@ -57,7 +63,9 @@ function FirstComponent() {
           actions.updateName({ name: 'billy', age: 16 });
         }}
       >
-        <div>{state.theDetail.name}</div>
+        <div className='text-gray-600 hover:text-gray-100'>
+          {state.theDetail.name}
+        </div>
       </div>
       <div>
         <div onClick={() => actions.incrementAge()}>{state.theDetail.age}</div>
